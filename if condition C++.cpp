5fa52fd3dcb07813f883ea734fc6e9cb82f44d39
Amrit Raj <121791502+amrit59@users.noreply.github.com>
2024-07-26 09:10:54 +0530

    Add files via upload

---
+++ if condition C++.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b;
+    cout<<"taking flaot as a input"<<endl;
+    cin>>a>>b;
+    if(a>0.01 && b>a){
+        cout<<"The condition is true";
+    }
+}
